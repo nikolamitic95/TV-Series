3fafdc4c1be88452582249171069c7c1c45a5a9a
Nikola Mitic <nikola.mitic1995@gmail.com>
2020-07-20 05:18:59 +0200

    Updates

---
+++ precache-manifest.1a8c5e80d18ad72c4c0eba0d15cb4a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52c060cad288731f543812ca4649996",
+    "url": "/TV-Series/index.html"
+  },
+  {
+    "revision": "57b6bebc4d80585d8ff6",
+    "url": "/TV-Series/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c7f3fd8e5e21a9a21611",
+    "url": "/TV-Series/static/css/main.3718f10f.chunk.css"
+  },
+  {
+    "revision": "57b6bebc4d80585d8ff6",
+    "url": "/TV-Series/static/js/2.c856c6f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TV-Series/static/js/2.c856c6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f3fd8e5e21a9a21611",
+    "url": "/TV-Series/static/js/main.f1671959.chunk.js"
+  },
+  {
+    "revision": "1372df6a7a80fd743733",
+    "url": "/TV-Series/static/js/runtime-main.31f7cb1d.js"
+  }
+]);
